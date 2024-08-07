55ad8f955b943cb59c26559a346d5ad291c622f2
EvilEnkidu <maganae@uabc.edu.mx>
2024-08-07 15:29:34 -0700

    Corrected first assignment, added control through functions

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
-pet1 = {
-	petName : "Cocoa Herrera",
-	age : 5,
+var pet1 = {
+    petName : "Charro",
+    age : 8,
 	gender : "F",
 	service : "Grooming",
 	breed : "Boxer"
 }
-pet2 = {
-	petName : "Bala",
-	age : 4,
+var pet2 = {
+    petName : "Apollo",
+	age : 7,
 	gender : "M",
 	service : "Check-up",
 	breed : "German Sheperd"
 }
-pet3 = {
-	petName : "Gala",
+var pet3 = {
+    petName : "Gala",
 	age : 2,
 	gender : "F",
 	service : "Check-up",
 	breed : "Mixed"
 }
 
-let pets = [pet1, pet2, pet3];
+var pets = [pet1, pet2, pet3];
 
-let petCount = pets.length;
+function showPetCount(){
+    let petCount = pets.length;
+    let button = document.getElementById("countBtn");
+    let h3 = document.createElement('h3');
+    h3.textContent = petCount + ' pets waiting!';
+    h3.id = "petCount";
+    button.parentNode.replaceChild(h3, button);
+    return true;
+}
 
-let petAgeAverage = parseFloat((pets[0].age + pets[1].age + pets[2].age)/pets.length).toFixed(2);
+function showAvgAge(){
+    let totalAge = 0;
+    for (let i = 0; i < pets.length; i++){
+        totalAge += pets[i].age;
+    }
+    const averageAge = totalAge / pets.length;
+    let button = document.getElementById("ageBtn");
+    let h3 = document.createElement('h3');
+    h3.textContent = parseFloat(averageAge).toFixed(1) + " years!";
+    h3.id = "petAge";
+    button.parentNode.replaceChild(h3, button);
+    return true;
+}
 
-let countDisplay = document.getElementById("petcount");
-countDisplay.innerHTML = petCount;
-let averageAgeDisplay = document.getElementById("average-age");
-averageAgeDisplay.innerHTML = petAgeAverage;
+function showPetNames(){
+    let button = document.getElementById('nameBtn');
+    let h3 = document.createElement('h3');
+    h3.textContent = '';
+
+    for (let i = 0; i < pets.length; i++){
+        h3.textContent += pets[i].petName + ' ';
+    }
+    h3.id = "petNames";
+    button.parentNode.replaceChild(h3, button);
+}
